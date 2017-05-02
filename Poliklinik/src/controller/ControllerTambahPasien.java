fb253dffa7a877268b8d0c7da627a18eb58d59db
dewasa98 <dewasa98@gmail.com>
2017-05-02 23:34:25 +0700

    controller
    tambah periksa pasien ruangan

---
+++ Poliklinik/src/controller/ControllerTambahPasien.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import view.TambahPasien;
+/**
+ *
+ * @author User
+ */
+public class ControllerTambahPasien implements ActionListener{
+
+    private TambahPasien view;
+    
+    public ControllerTambahPasien(){
+        view = new TambahPasien();
+        view.AddListener(this);
+        view.setVisible(true);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
